b33bf73d38c698932880807556ee2348ec27bef0
Elisha John Ragas <139514511+aerelish@users.noreply.github.com>
2026-03-17 23:18:02 +0800

    chore: prettier formatting

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import express from 'express'
-import helmet from 'helmet'
-import testRoutes from '@/routes/tests'
+import express from 'express';
+import helmet from 'helmet';
+import testRoutes from '@/routes/tests';
 import { errorHandler, notFoundHandler } from '@/middlewares/error-handler';
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(helmet());
-app.use(express.json())
+app.use(express.json());
 
-app.use(testRoutes)
+app.use(testRoutes);
 
 app.use(notFoundHandler);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+	console.log(`Server running on port ${PORT}`);
+});
--- src/routes/tests.ts
+++ src/routes/tests.ts
@@ -1,26 +1,25 @@
-import express from 'express'
+import express from 'express';
 import { AppError, UnauthorizedError } from '@/errors/errors';
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/health', (req, res) => {
-  res.json({ status: 'ok' })
-})
+	res.json({ status: 'ok' });
+});
 
 // throw a known operational error
 router.get('/test/unauthorized', (req, res, next) => {
-  next(new UnauthorizedError());
+	next(new UnauthorizedError());
 });
 
 // throw a non-operational error
 router.get('/test/crash', (req, res, next) => {
-  next(new AppError('DB pool exhausted', 500, false));
+	next(new AppError('DB pool exhausted', 500, false));
 });
 
 // throw an unexpected programmer error
 router.get('/test/unhandled', (req, res, next) => {
-  next(new Error('Something totally unexpected'));
+	next(new Error('Something totally unexpected'));
 });
 
-
-export default router
+export default router;
