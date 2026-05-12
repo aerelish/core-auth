b37109cbe457085d9c77a8b56f236982d34e2bd1
Elisha John Ragas <139514511+aerelish@users.noreply.github.com>
2026-05-12 08:59:25 +0800

    fix: +test on authmiddleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,14 +1,23 @@
-import { NextFunction, Request } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { UnauthorizedError } from '@/errors/UnauthorizedError';
 import { verifyAccessToken } from '@/lib/token';
 
-export const authMiddleware = (req: Request, next: NextFunction) => {
+/**
+ * middleware to authenticate the user by verifying the access token via verifyAccessToken function
+ * @param req Request : the express request object
+ * @param next NextFunction : the next function to call
+ * @throws UnauthorizedError if the access token is not present or invalid
+ */
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+	// get the access token from the cookies : needs cookie-parser middleware to be used
 	const accessToken = req.cookies.accessToken;
 	if (!accessToken) {
 		throw new UnauthorizedError();
 	}
 	try {
+		// verify the access token and set the user in the request object
 		const decoded = verifyAccessToken(accessToken);
+		// NOTE: req.user { id: { userId, iat, exp }} - this is the JWT payload shape
 		req.user = { id: decoded };
 		next();
 	} catch (error) {
--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -4,11 +4,17 @@
  */
 
 import express from 'express';
+import { authMiddleware } from '@/middlewares/auth';
 import { register, login } from './controller';
 
 const auth = express.Router();
 
 auth.post('/register', register);
 auth.post('/login', login);
+
+// for testing the auth middleware
+auth.get('/me', authMiddleware, (req, res) => {
+	res.json({ user: req.user });
+});
 
 export default auth;
